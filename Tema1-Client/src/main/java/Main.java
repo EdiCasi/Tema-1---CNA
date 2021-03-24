ce1324038932048a1d0d6c13c48772797364a8ba
EdiCasian <edi_herciu@yahoo.com>
2021-03-24 11:06:12 +0200

    mesaj

--- Tema1-Client/src/main/java/Main.java
+++ Tema1-Client/src/main/java/Main.java
@@ -1,2 +1,76 @@
-package PACKAGE_NAME;public class Main {
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import proto.PersonGrpc;
+import proto.PersonOuterClass;
+
+import java.util.Scanner;
+
+public class Main
+{
+
+
+    public static void main(String[] args)
+    {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress
+                ("localhost", 8999).usePlaintext().build();
+
+        PersonGrpc.PersonStub personStub = PersonGrpc.newStub(channel);
+
+        System.out.println("1) Add a person");
+        System.out.println("0) Exit");
+
+        int option = -1;
+        while (option != 0)
+        {
+            
+            System.out.print("Introduceti o optiune: ");
+            Scanner sc=new Scanner(System.in);
+            option=sc.nextInt();
+
+
+            switch(option)
+            {
+                case 1:
+                    Scanner read = new Scanner(System.in);
+                    System.out.print("Introduceti numele: ");
+                    String nume = read.nextLine();
+                    System.out.print("Introduceti CNP: ");
+                    String CNP = read.nextLine();
+
+                    personStub.returnPersonData(
+                            PersonOuterClass.PersonRequest.newBuilder().setName(nume).setCNP(CNP).build(),
+                            new StreamObserver<PersonOuterClass.Reply>()
+                            {
+                                @Override
+                                public void onNext(PersonOuterClass.Reply reply)
+                                {
+                                    System.out.println(reply);
+                                }
+
+                                @Override
+                                public void onError(Throwable throwable)
+                                {
+                                    System.out.println("Error: " + throwable.getMessage());
+                                }
+
+                                @Override
+                                public void onCompleted()
+                                {
+
+                                }
+                            }
+                    );
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Nu stiu comanda asta!");
+                    break;
+            }
+
+            }
+
+        channel.shutdown();
+    }
 }
--- Tema1-Server/src/main/java/PersonImpl.java
+++ Tema1-Server/src/main/java/PersonImpl.java
@@ -8,6 +8,72 @@ public class PersonImpl extends PersonGrpc.PersonImplBase
     public void returnPersonData(PersonOuterClass.PersonRequest request,
                                  StreamObserver<PersonOuterClass.Reply> responseObserver)
     {
-        
+        PersonOuterClass.Reply.Builder reply = PersonOuterClass.Reply.newBuilder();
+
+        String message = errorMessage(request.getName(), request.getCNP());
+
+        if (message == null)
+        {
+            reply.setMessage("Nume: " + request.getName() + "; "
+                    + "Varsta: " + findAgeFromCNP(request.getCNP()) + "; "
+                    + "Gen: " + findGenderFromCNP(request.getCNP())).build();
+
+            System.out.println(reply.getMessage());
+        } else
+        {
+            reply.setMessage(message).build();
+            System.out.println(message);
+        }
+
+        responseObserver.onNext(reply.build());
+        responseObserver.onCompleted();
+
     }
+
+    private Integer findAgeFromCNP(String CNP)
+    {
+        Integer varsta;
+        varsta = Character.getNumericValue(CNP.charAt(1)) * 10 + Character.getNumericValue(CNP.charAt(2));
+
+        if (CNP.charAt(0) == '1' || CNP.charAt(0) == '2')
+            return 2021 - 1900 - varsta;
+
+        if (CNP.charAt(0) == '5' || CNP.charAt(0) == '6')
+            return 2021 - 2000 - varsta;
+
+        return null;
+    }
+
+    private String findGenderFromCNP(String CNP)
+    {
+        if (CNP.charAt(0) == '1' || CNP.charAt(0) == '5')
+            return "Male";
+
+        if (CNP.charAt(0) == '2' || CNP.charAt(0) == '6')
+            return "Female";
+
+        return null;
+    }
+
+    public String errorMessage(String name, String CNP)
+    {
+        if (CNP.length() != 13)
+            return "CNP dimmension is wrong";
+
+        if (CNP.charAt(0) != '1' && CNP.charAt(0) != '2' && CNP.charAt(0) != '5' && CNP.charAt(0) != '6')
+            return "Your CNP is wrong";
+
+        Integer varsta;
+        varsta = Character.getNumericValue(CNP.charAt(1)) * 10 + Character.getNumericValue(CNP.charAt(2));
+
+        if (name == null || name == "")
+            return "Name cannot be Empty";
+
+        if (!CNP.matches("[0-9]+"))
+            return "Only numeric values is allowed in CNP";
+
+        return null;
+
+    }
+
 }
